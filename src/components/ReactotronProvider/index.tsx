d67cb7474e77e53ebf993fd637dd73150358fca4
Richard Evans <rmevans9@gmail.com>
2019-12-14 21:50:28 -0500

    Post merge fixes

--- src/components/ReactotronProvider/index.tsx
+++ src/components/ReactotronProvider/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react"
 import styled, { ThemeProvider } from "styled-components"
 
-import theme from "../theme"
+import theme from "../../theme"
 
 const ReactotronContainer = styled.div`
   font-family: ${props => props.theme.fontFamily};
