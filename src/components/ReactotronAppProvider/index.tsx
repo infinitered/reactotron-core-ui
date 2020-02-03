4ca97d969aabca8e09fb1f639684f7adfa289a98
Richard Evans <rmevans9@gmail.com>
2020-02-02 21:46:38 -0500

    fix: Adjust font size down a little bit

--- src/components/ReactotronAppProvider/index.tsx
+++ src/components/ReactotronAppProvider/index.tsx
@@ -5,6 +5,7 @@ import theme from "../../theme"
 
 const ReactotronContainer = styled.div`
   font-family: ${props => props.theme.fontFamily};
+  font-size: 0.94em;
   width: 100%;
   height: 100%;
   user-select: none;
